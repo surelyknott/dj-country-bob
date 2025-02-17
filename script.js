fa7a3f94dabed3ff02f63af183f2c58560c369e4
Jack Knott <jackhknott@hotmail.co.uk>
2025-02-17 18:58:11 +0000

    script change

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
+// This is our site Java Script
+document.addEventListener('DOMContentLoaded', function() {
+  const affirmations = document.querySelectorAll('.affirmations li');
+  affirmations.forEach(affirmation => affirmation.style.display = 'none');
+
+  const buttons = document.querySelectorAll('.genreButtons button');
+  buttons.forEach(button => {
+    button.addEventListener('click', function() {
+      affirmations.forEach(affirmation => affirmation.style.display = 'none');
+      const randomIndex = Math.floor(Math.random() * affirmations.length);
+      affirmations[randomIndex].style.display = 'block';
+    });
+  });
+});
+
 import { Client, Functions } from 'appwrite';
 
 // Initialize Appwrite Client
